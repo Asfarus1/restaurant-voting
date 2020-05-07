657fc85b4357a2faab9de54f89bcd3467b4ac709
Asfarus1 <m.a.v.norm@mail.ru>
2020-05-07 17:31:47 +0300

    Initial commit

---
+++ src/test/java/voting/RestaurantVotingApplicationTests.java
@@ -0,0 +1,13 @@
+package voting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantVotingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
